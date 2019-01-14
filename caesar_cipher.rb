0075e1b0f5cd86236dba5ff3e915ad93f69ae9a5
jttmckee <wp.jason@hotmail.co.uk>
2019-01-14 21:10:37 +0000

    Found a bug and fixed

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,6 @@
 def caesar_cipher(stringInput, shift)
   letters = stringInput.split('')
+  shift = shift % 26
   letters.collect! do |letter|
     if letter <= 'z' and letter >= 'a'
       letterNum = letter.ord
@@ -7,8 +8,8 @@ def caesar_cipher(stringInput, shift)
       letter = letterNum.chr
 
       if letter > 'z'
-        newShift = letter - 'z'.ord
-        letterNum = 'a'.ord + newShift
+        newShift = (letter.ord) - ('z'.ord)
+        letterNum = 'a'.ord + newShift - 1
         letter = letterNum.chr
       end
     elsif letter <= 'Z' and letter >='A'
@@ -16,8 +17,8 @@ def caesar_cipher(stringInput, shift)
       letterNum += shift
       letter = letterNum.chr
       if letter > 'Z'
-        newShift = letter - 'Z'.ord
-        letterNum = 'A'.ord + newShift
+        newShift = (letter.ord) - ('Z'.ord)
+        letterNum = 'A'.ord + newShift - 1
         letter = letterNum.chr
 
       end
@@ -27,5 +28,4 @@ def caesar_cipher(stringInput, shift)
   return letters.join
 
 end
-
 puts caesar_cipher("Test 123", 2)
